65d9721140a31b54a6b0519df2048bf423e2a909
luna <iszychen@gmail.com>
2020-12-18 22:53:01 +0800

    mysql my.ini

--- frp/linux/auto_start.sh
+++ frp/linux/auto_start.sh
@@ -7,7 +7,7 @@ StartLimitInterval=0
  
 [Service]
 ExecStart=/home/luna/Desktop/frp/frpc -c /home/luna/Desktop/frp/frpc.ini 
-ExecStop=killall /home/luna/Desktop/frp/frpc
+ExecStop=killall /home/luna/Desktop/frp/frpc    
 
 [Install]
 WantedBy=multi-user.target
@@ -16,6 +16,7 @@ EOF
 
 # 防火墙
 sudo ufw allow 5900
+sudo ufw allow 22
 # 开机自启动
 sudo systemctl enable frpc
 # 重新加载systemctl
