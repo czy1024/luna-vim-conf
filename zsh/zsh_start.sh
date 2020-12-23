bebc655f14d41ec835ccdf5ae9a006b0604816d2
luna <iszychen@gmail.com>
2020-12-23 14:08:59 +0800

    zsh

--- zsh/zsh_start.sh
+++ zsh/zsh_start.sh
@@ -1,5 +1,8 @@
 echo "安装zsh"
 sudo apt install -y zsh
+
+# 证书类问题
+apt-get install ca-certificates
 # 直接脚本安装
 # sh -c "$(curl  -fsSL http://lunacute.vaiwan.com/chfs/shared/Documents/luna-linux-conf/zsh/install.sh)"
 
@@ -15,3 +18,11 @@ sudo apt install -y zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc	
 chsh -s /bin/zsh 
+
+
+# 安装自动提示插件
+curl  -LJO  http://mimosa-pudica.net/src/incr-0.2.zsh
+mkdir ~/.oh-my-zsh/plugins/incr
+mv incr-0.2.zsh ~/.oh-my-zsh/plugins/incr
+echo 'source ~/.oh-my-zsh/plugins/incr/incr*.zsh' >> ~/.zshrc
+sudo source ~/.zshrc
