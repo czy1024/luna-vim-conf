a6f72259dbc1417e143cb3525bd4bf1f52577674
luna <iszychen@gmail.com>
2021-02-01 22:02:50 +0800

    'auto-deploy'

--- node.js/centos/npm.sh
+++ node.js/centos/npm.sh
@@ -2,3 +2,12 @@
 yum install npm -y
 echo "npm 配置淘宝镜像"
 npm config set registry https://registry.npm.taobao.org
+
+
+# Install Node.js
+curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
+sudo bash n lts
+
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+npm install -g n
