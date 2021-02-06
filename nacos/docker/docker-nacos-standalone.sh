cbc26ff47fc1eba128f118c4e897f5e6d190d829
luna <iszychen@gmail.com>
2021-02-06 15:50:17 +0800

    auto-deploy

--- nacos/docker/docker-nacos-standalone.sh
+++ nacos/docker/docker-nacos-standalone.sh
@@ -7,12 +7,12 @@ docker run -d \
 -p 8848:8848 \
 -e MODE=standalone \
 -e PREFER_HOST_MODE=hostname \
--e MYSQL_SERVICE_HOST=192.168.1.104 \
+-e MYSQL_SERVICE_HOST=192.168.65.1 \
 -e MYSQL_SERVICE_DB_NAME=nacos_devtest \
 -e MYSQL_SERVICE_PORT=3306 \
 -e MYSQL_SERVICE_USER=nacos \
 -e MYSQL_SERVICE_PASSWORD=nacos \
 -e NACOS_SERVER_PORT=8848 \
--e JVM_XMS=256m -e JVM_XMX=512m  \
+-e JVM_XMS=256m -e JVM_XMX=256m  \
 nacos/nacos-server
 
