b42fd3406dbfbacb28e01c422650b18793e16c7c
luna <iszychen@gmail.com>
2021-04-27 13:50:00 +0800

    auto-deploy

---
+++ frp/frps/auto_start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+rm -rf /usr/bin/frp
+rm -rf /etc/frp
+sudo cp /root/frp/frps /usr/bin/frp
+sudo chmod -R 777 /usr/bin/frps
+sudo mkdir /etc/frp
+sudo cp /root/frp/frps.ini /etc/frp/frps.ini
+sudo cat  > /lib/systemd/system/frps.service <<EOF
+[Unit]
+Description=The nginx HTTP and reverse proxy server
+After=network.target remote-fs.target nss-lookup.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/frps -c /etc/frp/frps.ini
+KillSignal=SIGQUIT
+TimeoutStopSec=5
+KillMode=process
+PrivateTmp=true
+StandardOutput=syslog
+StandardError=inherit
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+systemctl daemon-reload
+# 开机自启动
+sudo systemctl enable frps
+# 重新加载systemctl
+sudo systemctl daemon-reload
+systemctl start frps 
+systemctl status frps
+
