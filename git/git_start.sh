6ecc122a51a666e3b8c81eea01ae68382a10973c
luna <iszychen@gmail.com>
2021-06-17 14:54:41 +0800

    auto-deploy

--- git/git_start.sh
+++ git/git_start.sh
@@ -38,11 +38,13 @@ git config --global https.proxy http://192.168.1.100:10822
 # 修改地址
 git remote set-url origin xxx
 
-
 git clone https://gitclone.com/github.com/tendermint/tendermint.git
 
 git config --global url."https://gitclone.com/".insteadOf https://
 
+git verify-pack -v .git/objects/pack/pack-*.idx | sort -k 3 -g | tail -10
+
+git rev-list --objects --all | grep 9000f707aab19df57b5bafe9f2d4478cb07ca0b1
 
 git filter-branch -f --prune-empty --index-filter 'git rm -rf --cached --ignore-unmatch proxy/v2ray-linux-64.zip' --tag-name-filter cat -- --all
 
